d4d79e1e903e8e2994f5e69e7b8c366cbef92f8f
arnononline <arnononline@me.com>
2014-08-31 13:48:42 +0700

    test production

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,26 +27,25 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 set :keep_releases, 5
-after "deploy", "deploy:cleanup"
 
 namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
   after :publishing, :restart
+  after :finishing, 'deploy:cleanup'
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+  # after :restart, :clear_cache do
+    # on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
-    end
-  end
+    # end
+  # end
 
 end
