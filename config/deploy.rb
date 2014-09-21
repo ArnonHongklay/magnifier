70830e6f83d5e0b4673a07c70066d3fe2754151f
arnononline <arnononline@me.com>
2014-09-21 23:56:55 +0700

    production

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock '3.1.0'
 
 set :application, 'production'
-set :repo_url, 'git@bitbucket.org:arnononline/production.git'
+set :repo_url, 'git@bitbucket.org:nonmadden/production.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/home/non/myapp'
+set :deploy_to, '/home/non/ohmpieng/project/production'
 
 # Default value for :scm is :git
 set :scm, :git
