cc29f13df6671ce4eb9ec3a606a5753392be8665
Non Madden <nonmadden@gmail.com>
2014-11-02 10:19:12 +0700

    test production

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,38 +4,16 @@ lock '3.1.0'
 set :application, 'production'
 set :repo_url, 'https://nonmadden:3618b7120d511b2c1aa28fe61f14c50023143694@github.com/nonmadden/production.git'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-# set :branch,          "origin/master"
-
-# Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/deployer/project/production'
+set :pty, true
+set :format, :pretty
 set :use_sudo, true
 set :user, "deployer"
-# Default value for :scm is :git
 set :scm, :git
 set :rails_env, 'production'
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
 set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
 set :tmp_dir, "#{fetch(:home)}/tmp"
-
-# Default value for default_env is {}
 set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
-
-# Default value for keep_releases is 5
 set :keep_releases, 5
 
 namespace :deploy do
