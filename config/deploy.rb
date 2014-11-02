f1156e666f4d6b3b24a40a54862c3dae5754a073
Non Madden <nonmadden@gmail.com>
2014-11-02 10:44:29 +0700

    test production

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,10 +17,10 @@ set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
 set :keep_releases, 5
 
 namespace :deploy do
-  before "deploy:assets:precompile" do
+  before :deploy, "deploy:assets:precompile" do
     run [
       # "ln -nfs #{shared_path}/config/settings.yml #{release_path}/config/settings.yml",
-      "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml",
+      "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
       # "ln -fs #{shared_path}/uploads #{release_path}/uploads"
     ].join(" && ")
   end
