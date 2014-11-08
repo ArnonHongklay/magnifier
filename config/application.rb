313a84f74d0d62207e1bcfe529eaf7fa10c47392
Non Madden <nonmadden@gmail.com>
2014-11-09 00:58:35 +0700

    root dir

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,7 @@ module Production
     # Enable caching via the memory store
     config.cache_store = :memory_store
 
+    config.root_directory = '/apps'
+    config.assets.prefix = '/apps/assets/'
   end
 end
