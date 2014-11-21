1c52e312e28701df61646f0932d126c4f1d4bce2
Non Madden <nonmadden@gmail.com>
2014-11-22 05:25:31 +0700

    update landing design

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( landing.js )
 Rails.application.config.assets.precompile += %w( landing.css )
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
