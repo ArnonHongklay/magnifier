f47c9f90f7bae533d4d55aa9bcdd8c93ba19aafb
Non Madden <nonmadden@gmail.com>
2015-01-15 03:49:35 +0700

    updated

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,7 @@
 class LandingController < ApplicationController
   def index
     if signed_in?
-    	raise account_index_path(params[:account_id])
+      raise account_index_path(params[:account_id])
       # redirect_to :account_index_path
     end
   end
