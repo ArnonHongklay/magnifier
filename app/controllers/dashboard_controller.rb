2191f5aad9dcd3bb49adad715289797e550cf5c6
Non Madden <nonmadden@gmail.com>
2015-01-19 22:23:40 +0700

    xxx

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,21 @@
 class DashboardController < ApplicationController
+
+  def home
+  end
+
   def index
+
+  end
+
+  def monitoring_tools
+
+  end
+
+  def ticket_support
+
+  end
+
+  def report
+
   end
 end
