0e1ce15e7b7fb89994634c37ba632cf3b58284a3
nonmadden <non@page365.net>
2015-04-18 22:32:16 +0700

    fix indent assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,10 @@
 
 Rails.application do |config|
-	config.assets.version = '1.0'
-	config.assets.initialize_on_precompile = true
-	config.assets.paths << Rails.root.join("vendor", "assets", "fonts", "bower_components")
-	config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
-	config.assets.precompile += %w( .css )
-	config.assets.precompile += %w( .js )
-	config.assets.precompile += %w( .png .jpg .jpeg .gif .woff .ttf .svg .eot )
+  config.assets.version = '1.0'
+  config.assets.initialize_on_precompile = true
+  config.assets.paths << Rails.root.join("vendor", "assets", "fonts", "bower_components")
+  config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+  config.assets.precompile += %w( .css )
+  config.assets.precompile += %w( .js )
+  config.assets.precompile += %w( .png .jpg .jpeg .gif .woff .ttf .svg .eot )
 end
-
