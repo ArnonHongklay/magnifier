0689f085b70bbcb99b2c5dcd7658e8814a518c66
nonmadden <nonmadden@gmail.com>
2015-04-18 22:29:36 +0700

    Fix it

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,11 @@
-# Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
-# Rails.application.config.assets.initialize_on_precompile = true
+Rails.application do |config|
+	config.assets.version = '1.0'
+	config.assets.initialize_on_precompile = true
+	config.assets.paths << Rails.root.join("vendor", "assets", "fonts", "bower_components")
+	config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+	config.assets.precompile += %w( .css )
+	config.assets.precompile += %w( .js )
+	config.assets.precompile += %w( .png .jpg .jpeg .gif .woff .ttf .svg .eot )
+end
 
-Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "fonts", "bower_components")
-Rails.application.config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
-Rails.application.config.assets.precompile += %w( .css )
-Rails.application.config.assets.precompile += %w( .js )
-Rails.application.config.assets.precompile += %w( .png .jpg .jpeg .gif .woff .ttf .svg .eot )
