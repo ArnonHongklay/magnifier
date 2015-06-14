635ba3090afbd9ada17e7a490672e34f2a1b4389
nonmadden <nonmadden@gmail.com>
2015-06-14 14:02:25 +0700

    fix source

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,6 +1,6 @@
 HighVoltage.configure do |config|
   config.routes = false
-	config.content_path = 'accounts/'
-	config.page_caching = true
-	config.action_caching = true
+  config.content_path = 'accounts/'
+  # config.page_caching = true
+  # config.action_caching = true
 end
