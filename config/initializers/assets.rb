a7c5dc9cb16ffb70b29b41773b84c93e3500efd5
-_-! <nonmadden@gmail.com>
2015-07-01 20:51:41 +0700

    assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@ Rails.application.config.assets.version = '1.0'
   # config.assets.initialize_on_precompile = true
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
 # Rails.application.config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
-Rails.application.config.assets.precompile += %w( .css )
-Rails.application.config.assets.precompile += %w( .js )
-Rails.application.config.assets.precompile += %w( .png .jpg .jpeg .gif .woff .ttf .svg .eot )
+Rails.application.config.assets.precompile += %w( *.css )
+Rails.application.config.assets.precompile += %w( *.js )
+Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif ) 
+Rails.application.config.assets.precompile += %w( *.woff *.ttf *.svg *.eot )
