ac3066f8912f6c94750972090f27224f675dde4f
-_-! <nonmadden@gmail.com>
2015-07-04 07:16:18 +0700

    config

--- Gemfile
+++ Gemfile
@@ -1,49 +1,31 @@
 source 'https://rubygems.org'
 ruby '2.2.0'
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.0'
-gem 'nokogiri'
-# Use mysql as the database for Active Record
+gem 'rails', '4.2.3'
 gem 'mysql2'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
-# bundle exec rake doc:rails generates the API under doc/api.
-gem 'sdoc', '~> 0.4.0',          group: :doc
-gem 'activeadmin', github: 'activeadmin'
+
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'therubyracer', platforms: :ruby
+
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'bcrypt', '~> 3.1.7'
+
+gem 'nokogiri'
+gem 'activeadmin',                github: 'activeadmin'
 gem 'activerecord-session_store', github: 'rails/activerecord-session_store'
-gem 'activeresource', github: 'rails/activeresource'
+gem 'activeresource',             github: 'rails/activeresource'
 gem "activerecord-deprecated_finders", "~> 1.0.3"
 gem "actionpack-action_caching"
 gem "actionpack-page_caching"
+
 # static page
 gem 'high_voltage'
-# bundle
-# gem 'bower-rails'
 
-# javascript
-gem 'angular-rails-templates'
-gem 'coffee-rails', github: 'rails/coffee-rails'
-gem 'nprogress-rails'
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
-
-# stylesheet
-gem 'sass-rails'
-# gem 'bootstrap-sass'
-gem 'bootstrap-sass'
-gem "font-awesome-rails"
-gem 'autoprefixer-rails', '5.0.0.3'  # CSS auto-prefixing
-gem "compass-rails", github: 'Compass/compass-rails' # CSS helper
-# gem 'sprockets-rails', require: 'sprockets/railtie'
-gem "sprockets"
-# Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0' # Minification
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder'
-gem 'wicked_pdf'  # pdf
-
-# gem 'strong_parameters'
 gem 'cancan'                            # => Authorization
 gem 'devise'                            # => Authorization
 gem 'koala'                             # => Facebook
@@ -57,35 +39,35 @@ gem 'sidekiq-failures'
 gem 'sinatra', :require => nil
 
 gem 'redis'
-# gem 'redis-namespace'                 # Cleaner redis keys
-# gem 'redis-semaphore'                 # Facebook access locking
-
 gem 'websocket-rails'
-# gem 'tubesock'
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
 
-group :development do
-  gem 'puma'                            # Multi-threaded server for development
-  gem 'foreman'                         # Executes Procfile
-  gem 'capistrano-ext'                  # So developers and CircleCI can deploy
-  gem 'capistrano', '~> 3.1.0'          # So developers and CircleCI can deploy
-  gem 'capistrano-bundler', '~> 1.1.2'
-  gem 'capistrano-bower'
-  gem 'capistrano-rails', '~> 1.1.1' # Use Capistrano for deployment
-  gem 'capistrano-rbenv', github: "capistrano/rbenv"
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
-end
+gem 'nprogress-rails'
+gem 'sass-rails', '~> 5.0'
+gem 'bootstrap-sass'
+gem "font-awesome-rails"
+
+gem 'autoprefixer-rails', '5.0.0.3'  # CSS auto-prefixing
+gem "compass-rails", github: 'Compass/compass-rails' # CSS helper
+gem "sprockets"
+
+gem 'wicked_pdf'
 
 group :development, :test do
-  gem 'rspec-rails'               # Testing engine
-  gem 'rspec-its'                 # Rspec deprecated its
-  gem 'rspec-collection_matchers' # Rspec deprecated should
+  gem 'puma'                            # => Multi-threaded server for development
+  gem 'foreman'                         # => Executes Procfile
+  gem 'capistrano'                      # => So developers and CircleCI can deploy
+  gem 'capistrano-ext'                  # => So developers and CircleCI can deploy
+  gem 'capistrano-rails'                # =>  Use Capistrano for deployment
+  gem 'capistrano-rbenv',               github: "capistrano/rbenv"
+  gem 'capistrano-bundler'
+
+  gem 'byebug'
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+
+  gem 'rspec-rails'               # => Testing engine
+  gem 'rspec-its'                 # => Rspec deprecated its
+  gem 'rspec-collection_matchers' # => Rspec deprecated should
 end
 
 group :production do
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,49 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.2.0'
 
 set :application, 'production'
-
-set :scm, :git
 set :repo_url, 'git@github.com:ohmpieng/production.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :branch, 'master'
 
+# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deploy/production'
 set :deploy_user, 'deploy'
 set :use_sudo, true
 
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for :format is :pretty
+set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :debug
+
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
 set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
 
-set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp vendor/bundle public/system}
-set :tmp_dir, "#{fetch(:home)}/tmp"
-set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
+# Default value for keep_releases is 5
 set :keep_releases, 5
-set :pty, true
-set :format, :pretty
 
 set :stages, ["staging", "production"]
 set :default_stage, "production"
 
-# set :bower_flags, '--quiet --config.interactive=false'
-# set :bower_roles, :web
-# set :bower_target_path, "#{release_path}/web"
-# set :bower_bin, '/usr/local/bin/bower'
-
 namespace :deploy do
-  before :publishing, :setup_config
-  # after :publishing, :restart
-  after :finishing, 'deploy:cleanup'
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -43,49 +54,65 @@ namespace :deploy do
     end
   end
 
-  # desc 'Restart application'
-  #   task :restart do
-  #     on roles(:app), in: :sequence, wait: 5 do
-  #       execute :touch, release_path.join('tmp/restart.txt')
-  #   end
-  # end
-
-  desc 'Runs rake db:seed for SeedMigrations data'
-  task :seed => [:set_rails_env] do
-    on primary fetch(:migration_role) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, "db:seed"
-        end
-      end
-    end
-  end
-
-  after 'deploy:migrate', 'deploy:seed'
-
-  desc "Database config"
-    task :setup_config do
-      on roles(:app), in: :sequence, wait: 5 do
-        execute "mkdir -p #{shared_path}/config"
-        execute "mkdir -p #{shared_path}/bin"
-        execute "mkdir -p #{shared_path}/log"
-        execute "mkdir -p #{shared_path}/tmp"
-        execute "mkdir -p #{shared_path}/vendor/bundle"
-        execute "mkdir -p #{shared_path}/public/system"
-        upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
-        # upload! StringIO.new(File.read("config/config.yml")), "#{shared_path}/config/config.yml"
-    end
-  end
 end
 
-namespace :bower do
-  desc 'Install bower'
-  task :install do
-    on roles(:web) do
-      within release_path do
-        execute :rake, 'bower:install CI=true'
-      end
-    end
-  end
-end
-# before 'deploy:compile_assets', 'bower:install'
+# namespace :deploy do
+#   before :publishing, :setup_config
+#   # after :publishing, :restart
+#   after :finishing, 'deploy:cleanup'
+
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
+
+#   # desc 'Restart application'
+#   #   task :restart do
+#   #     on roles(:app), in: :sequence, wait: 5 do
+#   #       execute :touch, release_path.join('tmp/restart.txt')
+#   #   end
+#   # end
+
+#   desc 'Runs rake db:seed for SeedMigrations data'
+#   task :seed => [:set_rails_env] do
+#     on primary fetch(:migration_role) do
+#       within release_path do
+#         with rails_env: fetch(:rails_env) do
+#           execute :rake, "db:seed"
+#         end
+#       end
+#     end
+#   end
+
+#   after 'deploy:migrate', 'deploy:seed'
+
+#   desc "Database config"
+#     task :setup_config do
+#       on roles(:app), in: :sequence, wait: 5 do
+#         execute "mkdir -p #{shared_path}/config"
+#         execute "mkdir -p #{shared_path}/bin"
+#         execute "mkdir -p #{shared_path}/log"
+#         execute "mkdir -p #{shared_path}/tmp"
+#         execute "mkdir -p #{shared_path}/vendor/bundle"
+#         execute "mkdir -p #{shared_path}/public/system"
+#         upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
+#         # upload! StringIO.new(File.read("config/config.yml")), "#{shared_path}/config/config.yml"
+#     end
+#   end
+# end
+
+# namespace :bower do
+#   desc 'Install bower'
+#   task :install do
+#     on roles(:web) do
+#       within release_path do
+#         execute :rake, 'bower:install CI=true'
+#       end
+#     end
+#   end
+# end
+# # before 'deploy:compile_assets', 'bower:install'
