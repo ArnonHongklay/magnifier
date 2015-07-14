d9de51589650312fb6cc8212a29e4530bf93304b
-_-! <nonmadden@gmail.com>
2015-07-14 23:32:51 +0700

    fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,16 +1,18 @@
+Rails.application.configure do
 
-# Enable the asset pipeline
-Rails.application.config.assets.enabled = true
-Rails.application.config.assets.version = '1.1' # Version of your assets, change this if you want to expire all your assets
-Rails.application.config.assets.initialize_on_precompile = false
-Rails.application.config.sass.preferred_syntax = :sass
-Rails.application.config.sass.line_comments = false
-Rails.application.config.sass.cache = false
+  # Enable the asset pipeline
+  config.assets.enabled = true
+  config.assets.version = '1.1' # Version of your assets, change this if you want to expire all your assets
+  config.assets.initialize_on_precompile = false
+  config.sass.preferred_syntax = :sass
+  config.sass.line_comments = false
+  config.sass.cache = false
 
-Rails.application.config.assets.debug = true
+  config.assets.debug = true
 
-Rails.application.config.assets.paths << Rails.root.join('components')
-Rails.application.config.assets.precompile += %w( *.css *.js )
-Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif *.ico )
-Rails.application.config.assets.precompile += %w( *.woff *.ttf *.svg *.eot )
-# Rails.application.config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/
+  config.assets.paths << Rails.root.join('components')
+  config.assets.precompile += %w( *.css *.js )
+  config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif *.ico )
+  config.assets.precompile += %w( *.woff *.ttf *.svg *.eot )
+  # config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/
+end
