a5aced3af6ea3ef9c96fe9b668f056457a02dcc5
-_-! <non@page365.net>
2015-07-17 20:39:04 +0700

    fix relations

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,10 +1,10 @@
 class Server < ActiveRecord::Base
-  has_many :accounts
-  has_many :cpus
-  has_many :memories
-  has_many :disks
-  has_many :arps
-  has_many :bandwidths
-  has_many :ios
-  has_many :load_avgs
+  belongs_to :account
+  belongs_to :cpus
+  belongs_to :memories
+  belongs_to :disks
+  belongs_to :arp
+  belongs_to :bandwidths
+  belongs_to :ios
+  belongs_to :load_avgs
 end
