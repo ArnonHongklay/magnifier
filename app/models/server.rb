5ff96a4f5e98984e95ea44cb6ee1d2389eee0e7f
-_-! <non@page365.net>
2015-07-17 23:14:35 +0700

    fix all

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,10 +1,10 @@
 class Server < ActiveRecord::Base
   belongs_to :account
-  belongs_to :cpus
-  belongs_to :memories
-  belongs_to :disks
+  belongs_to :cpu
+  belongs_to :memory
+  belongs_to :disk
   belongs_to :arp
-  belongs_to :bandwidths
-  belongs_to :ios
-  belongs_to :load_avgs
+  belongs_to :bandwidth
+  belongs_to :io
+  belongs_to :load_avg
 end
