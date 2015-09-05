5b7e547b72fe638ab646eb8876b2e22b16e30195
-_-! <nonmadden@gmail.com>
2015-09-05 20:43:58 +0700

    no sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/0' }
-  config.failures_max_count = 5000
-  config.average_scheduled_poll_interval = 15
-end
+#
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://127.0.0.1:6379/0' }
+#   config.failures_max_count = 5000
+#   config.average_scheduled_poll_interval = 15
+# end
