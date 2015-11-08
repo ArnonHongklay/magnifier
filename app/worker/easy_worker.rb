5108caef9345a2efceb54052d5bd623ae574f2b3
nonmadden <nonmadden@gmail.com>
2015-11-08 13:18:13 +0700

    fix worker

--- app/worker/easy_worker.rb
+++ app/worker/easy_worker.rb
@@ -1,55 +1,19 @@
-#
-# class EasyWorker
-#
-#   def self.queue
-#     :sloth
-#   end
-#
-#   def self.perform(server_id)
-#     server = Server.find(server_id)
-#
-#     EasyWorker.update_server(server)
-#   end
-#
-#   def self.poller
-#     Account.all.each do |account|
-#       next if account.servers.empty?
-#       account.servers.each do |server|
-#         next if server.ip_address.nil?
-#         perform_async(server.id)
-#       end
-#     end
-#   end
-#
-#   def self.update_server(server)
-#     url = "http://#{server.ip_address}:9999/general_info"
-#     response = Net::HTTP.get_response(URI.parse(url))
-#     item = JSON.parse(response.body)
-#
-#     server.os       = item['OS']
-#     server.hostname = item['Hostname']
-#     server.uptime   = item['Uptime']
-#     server.datetime = item['Server Time']
-#     server.save!
-#   end
-# end
-
 class EasyWorker
   include Sidekiq::Worker
-  # sidekiq_options queue: :critical, retry: true
+  sidekiq_options :retry => false
 
   def perform(server_id)
     Account.all.each do |account|
       next if account.servers.empty?
       account.servers.each do |server|
         next if server.ip_address.nil?
-        update_server(server.id)
+        update_server(server)
       end
     end
   end
 
   def self.update_server(server)
-    url = "http://#{server.ip_address}:9999/general_info"
+    url = "http://#{server.ip_address}:1337/general_info"
     response = Net::HTTP.get_response(URI.parse(url))
     item = JSON.parse(response.body)
 
